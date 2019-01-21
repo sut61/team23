1b34e7cfe2aad811bea4c81fa7a822e73323f7d1
Grospector <pichakorn50868@hotmail.com>
2019-01-21 17:48:41 +0700

    เพิ่ม alert

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertService } from '../alert.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertService: AlertService) { }
 
   ngOnInit() {
   }
 
+  success(message: string) {
+          this.alertService.success(message);
+      }
+
+      error(message: string) {
+          this.alertService.error(message);
+      }
+
+      info(message: string) {
+          this.alertService.info(message);
+      }
+
+      warn(message: string) {
+          this.alertService.warn(message);
+      }
+
+      clear() {
+          this.alertService.clear();
+      }
 }
