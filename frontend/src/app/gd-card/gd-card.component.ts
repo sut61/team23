27dcb8f8816a4787af83dcc09a75a53f0b1441a6
weerapata1 <seal_recon_00@htomail.com>
2019-02-18 00:49:58 +0700

    update FN

--- frontend/src/app/gd-card/gd-card.component.ts
+++ frontend/src/app/gd-card/gd-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild,Inject  } from '@angular/core';
 import {DatePipe} from '@angular/common';
 import { GoldcardService } from '../goldcard.service';
 import {ErrorStateMatcher} from '@angular/material/core';
@@ -9,9 +9,11 @@ import { HttpClient} from '@angular/common/http';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import { AlertService } from '../alert.service';
 import { AuthService } from '../auth/auth.service';
-import {MatTableDataSource} from '@angular/material';
+import { AlertService } from '../alert.service';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material';
+import {GDC} from './gdc'
 
 @Component({
   selector: 'app-gd-card',
@@ -23,31 +25,56 @@ export class GDCardComponent implements OnInit {
   hospitals : Array<any>;
   members : Array<any>;
   officers : Array<any>;
+  usernames : Array<any>;
   select: any = {
           officername: '',
           hospitalname: '',
-          membername:  '',
+          usermane:  '',
     };
   input: any = {
           detail: ''
   }
 
+  constructor(private formbuilder:FormBuilder,private alertService:AlertService, private goldcardService: GoldcardService , private bottomSheet: MatBottomSheet, private httpClient: HttpClient,private route: ActivatedRoute,
+   private router: Router ){}
 
-  constructor(private goldcardService: GoldcardService, private httpClient: HttpClient,private route: ActivatedRoute,
-   private router: Router ) {}
+  isValidFormSubmitted = null;
+  Detail = "^\\S.+";
+  gdcForm  = this.formbuilder.group({
+    officerName_no : ['', [Validators.required, Validators.pattern('')]],
+    hospitalName_no: ['', [Validators.required, Validators.pattern('')]],
+    username_no: ['', [Validators.required, Validators.pattern('')]],
+    detail_no : ['', [Validators.required, Validators.pattern(this.Detail)]],
+  });
+  onFormSubmit(){
+        this.isValidFormSubmitted = false;
+        if (this.gdcForm.invalid) {
+                  this.alertService.error('กรุณา กรอกข้อมูล ให้ครบถ้วน');
+                  if(this.gdcForm.controls['detail_no'].hasError('pattern')){
+                                  this.alertService.error('กรุณากรอกรายละเอียดโดยห้ามขึ้นต้นด้วยช่องว่าง');
+                  }
+                  return;
+        }
+        else if (this.gdcForm.valid){
+                this.save(this.gdcForm.value);
+                this.isValidFormSubmitted = true;
+        }
+         this.isValidFormSubmitted = true;
+  }
 
-  save(){
+  save( gdc : GDC){
   // http://localhost:8080/Goldcard/{detail}/{officerName}/{HospialName}/{MemberName}
-              this.httpClient.post('http://localhost:8080/Goldcard/'+this.input.detail+'/'+this.select.officerName+'/'+this.select.hospitalname+'/'+this.select.membername,this.input)
+              this.httpClient.post('http://localhost:8080/Goldcard/'+gdc.detail_no+'/'+gdc.officerName_no+'/'+gdc.hospitalName_no+'/'+gdc.username_no
+              ,gdc)
                 .subscribe(
                     data => {
                         console.log('PUT Request is successful', data);
-                        alert('บันทึกเรียบร้อยแล้ว');
-this.router.navigate(['/reload/GDCard']);
+                        this.alertService.success('บันทึกเรียบร้อยแล้ว');
+
                     },
                     error => {
                         console.log('Error', error);
-                        alert('กรอกข้อมูลผิดพลาด โปรดตรวจสอบใหม่');
+                        this.alertService.success('กรอกข้อมูลผิดพลาด โปรดตรวจสอบใหม่');
                     }
                 );
 
@@ -66,6 +93,11 @@ this.router.navigate(['/reload/GDCard']);
   this.goldcardService.getOfficer().subscribe(data =>{
       this.officers = data;
       console.log(this.officers);
+      });
+
+  this.goldcardService.getRightRegistration().subscribe(data =>{
+      this.usernames = data;
+      console.log(this.usernames);
       });
 
   }
--- frontend/src/app/publicz/publicz.component.ts
+++ frontend/src/app/publicz/publicz.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild,Inject  } from '@angular/core';
 import {DatePipe} from '@angular/common';
 import { GoldcardService } from '../goldcard.service';
 import {ErrorStateMatcher} from '@angular/material/core';
@@ -12,6 +12,8 @@ import { BehaviorSubject } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 import { AlertService } from '../alert.service';
 import {MatPaginator, MatTableDataSource} from '@angular/material';
+import {MatBottomSheet, MatBottomSheetRef} from '@angular/material';
+import {Pub} from './pub';
 
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
@@ -27,7 +29,6 @@ isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | nu
   styleUrls: ['./publicz.component.css']
 })
 export class PubliczComponent implements OnInit {
-
   hospitals : Array<any>;
   officers : Array<any>;
   typeofpub : Array<any>;
@@ -49,28 +50,60 @@ export class PubliczComponent implements OnInit {
   lock: FormGroup;
   myControl = new FormControl();
 
-  constructor(private alertService:AlertService,private authService: AuthService,fb: FormBuilder,private goldcardService: GoldcardService, private httpClient: HttpClient,private router: Router){
-       this.lock = fb.group({
-          hideRequiredMarker: false,
-          floatLabel: 'never',
-       });
+  constructor(private formbuilder:FormBuilder,private alertService:AlertService, private goldcardService: GoldcardService , private bottomSheet: MatBottomSheet, private httpClient: HttpClient,private route: ActivatedRoute,
+   private router: Router ){}
+
+  isValidFormSubmitted = null;
+  Header = ".{5,20}";
+  Detail = "^\\S.+";
+  email  = "\\w{2,10}@\\w{2,10}\\.(com)||\\w{2,10}@\\w{2,10}\\.(\w{2,3}\\.\\w{2,3})||\\w{2,10}@\\w{2,10}\\.(COM)";
+  call   = "[0][896]\\d{8}"
+  pubForm  = this.formbuilder.group({
+    publiczHead_no : ['', [Validators.required, Validators.pattern(this.Header)]],
+    PublicizeDetail_no: ['', [Validators.required, Validators.pattern(this.Detail)]],
+    officerName_no: ['', [Validators.required, Validators.pattern('')]],
+    hospitalName_no : ['', [Validators.required, Validators.pattern('')]],
+    typePubName_no : ['', [Validators.required, Validators.pattern('')]],
+    email_no : ['', [Validators.required, Validators.pattern(this.email)]],
+    call_no : ['', [Validators.required, Validators.pattern(this.call)]],
+  });
+  onFormSubmit(){
+        this.isValidFormSubmitted = false;
+        if (this.pubForm.invalid) {
+                  this.alertService.error('กรุณา กรอกข้อมูล ให้ครบถ้วน');
+                  if(this.pubForm.controls['publiczHead_no'].hasError('pattern')){
+                                  this.alertService.error('กรุณากรอกหัวเรื่องให้มีความยาว 5 ถึง 20 ตัวอักษร');
+                  }else if(this.pubForm.controls['PublicizeDetail_no'].hasError('pattern')){
+                                  this.alertService.error('รายละเอียดห้ามมีช่องว่างหน้าตัวอักษร');
+                  }else if(this.pubForm.controls['email_no'].hasError('pattern')){
+                                  this.alertService.error('กรุณากรอกอีเมลติดต่อให้ถูกต้อง');
+                  }else if(this.pubForm.controls['call_no'].hasError('pattern')){
+                                  this.alertService.error('กรุณากรอกเบอร์โทรติดต่อให้ถูกต้อง');
+                  }
+                  return;
+        }
+        else if (this.pubForm.valid){
+                this.save(this.pubForm.value);
+                this.isValidFormSubmitted = true;
+        }
+         this.isValidFormSubmitted = true;
   }
 
-save(){
-this.alertService.clear();
-  // http://localhost:8080/Publicz/{publiczHead}/{PublicizeDetail}/{date_reg}/{officerName}/{hospialName}/{TypeOfPublicz}/{email}/{call}
-        this.httpClient.post('http://localhost:8080/Publicz/'+this.input.publiczHead+'/'+this.input.PublicizeDetail+'/'+this.pipe.transform(this.CurrentDate,'dd:MM:yyyy')+'/'+this.select.officerName+'/'+this.select.hospitalname+'/'+this.select.typeofpub+'/'+this.input.email+'/'+this.input.call
-        ,this.input)
-            .subscribe(
-              data => {
-                console.log('PUT Request is successful', data);
-                this.alertService.success('บันทึกสำเร็จ');
-              },
-              error => {
-                console.log('Error', error);
-                this.alertService.error('ไม่บันทึกสำเร็จ');
-              }
-            );
+  save(pub : Pub){
+    // http://localhost:8080/Publicz/{publiczHead}/{PublicizeDetail}/{date_reg}/{officerName}/{hospialName}/{TypeOfPublicz}/{email}/{call}
+          this.httpClient.post('http://localhost:8080/Publicz/'+pub.publiczHead_no+'/'+pub.PublicizeDetail_no+'/'+this.pipe.transform(this.CurrentDate,'dd:MM:yyyy')+'/'+pub.officerName_no+'/'+pub.hospitalName_no+'/'+pub.typePubName_no+'/'+pub.email_no+'/'+pub.call_no
+          ,pub)
+              .subscribe(
+                data => {
+                  console.log('PUT Request is successful', data);
+                  this.alertService.success('บันทึกสำเร็จ');
+
+                },
+                error => {
+                  console.log('Error', error);
+                  this.alertService.error('ไม่บันทึกสำเร็จ');
+                }
+              );
   }
 
   ngOnInit() {
@@ -85,7 +118,8 @@ this.alertService.clear();
     this.goldcardService.getTypeOfPublicz().subscribe(data =>{
       this.typeofpub = data;
       console.log(this.typeofpub);
-      });
+    });
+
   }
 
 }
