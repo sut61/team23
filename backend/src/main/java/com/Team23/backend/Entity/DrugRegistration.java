eec8bff979f1bc909de376a31220d9b55452eb40
nitikorn chumnankul <nitikornchumnankul@gmail.com>
2019-03-05 17:48:21 +0700

    เพิ่มเงื่อนไขการ test class รอบๆ sprint ที่ 1 และ 2 เสร็จแล้ว

--- backend/src/main/java/com/Team23/backend/Entity/DrugRegistration.java
+++ backend/src/main/java/com/Team23/backend/Entity/DrugRegistration.java
@@ -1,10 +1,13 @@
 package com.Team23.backend.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,6 +21,11 @@ public class DrugRegistration {
     @Id 
     @GeneratedValue(strategy= GenerationType.IDENTITY) //this isolate id 1 2 3
     private @NonNull Long drugRegistrationId;
+
+
+    @Pattern(regexp="[a-zA-Z]*|[ก-์]*" ,message="drugRegistrationName No have special character")
+    @Size(min=2,max=100,message="drugRegistrationName should not have alphabet at less 2 alphabet and than 30 alphabet")
+    @Column(unique = true)
     @NotNull(message="drugRegistrationName must not be null to be valid")
     private String drugRegistrationName;
 
--- backend/src/main/java/com/Team23/backend/Entity/TypesOfDosageForms.java
+++ backend/src/main/java/com/Team23/backend/Entity/TypesOfDosageForms.java
@@ -1,10 +1,13 @@
 package com.Team23.backend.Entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,6 +21,13 @@ public class TypesOfDosageForms {
         @Id 
         @GeneratedValue(strategy= GenerationType.IDENTITY) //this isolate id 1 2 3
         private @NonNull Long typesOfDosageFormsId;
+
+
+
+        
+        @Pattern(regexp="[a-zA-Z]*|[ก-์]*" ,message="typesOfDosageFormsName No have special character")
+        @Size(min=2,max=100,message="typesOfDosageFormsName should not have alphabet at less 2 alphabet and than 30 alphabet")
+        @Column(unique = true)
         @NotNull(message="typesOfDosageFormsName must not be null to be valid")
         private String typesOfDosageFormsName;
 public TypesOfDosageForms(){}
